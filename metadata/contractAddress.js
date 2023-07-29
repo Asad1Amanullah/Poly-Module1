131e963e9141f44d3c1ac361e410cb5adf3aa77d
Asad1Amanullah <96766742+Asad1Amanullah@users.noreply.github.com>
2023-07-29 11:10:24 +0530

    Add files via upload

---
+++ metadata/contractAddress.js
@@ -0,0 +1,3 @@
+
+    export const nftAddress = "0x478002A12b40A42cC55A00a7D7433756CD07Cdd2"
+  
---
+++ metadata/nfts.js
@@ -0,0 +1,29 @@
+const nfts = [
+  {
+    name: "0",
+    description: "Ghost",
+    image: "QmUqdQHC2PYA22UXFusbt9XSwiUYP8oMom9guvhk5YYcRG",
+  },
+  {
+    name: "1",
+    description: "Ghost",
+    image: "QmYoFb2efoVRbdBjiMMh9p3QNYYZKgZt4ZYjLBYX7rLLYd",
+  }, //change
+  {
+    name: "2",
+    description: "Ghost",
+    image: "QmTSpcPhprA5ptVt88cx7epypsx4P5rA1zcezeeg4Hws5g",
+  },
+  {
+    name: "3",
+    description: "Ghost",
+    image: "QmaxZ7sZWVovdpB65ktjoPAetw2Wq4Bb2zmhBtDJXubb7g",
+  },
+  {
+    name: "4",
+    description: "Ghost.",
+    image: "QmaGmDLLH1NFVf9Fy4xhojqzdLNLffMUM7kir24CaMw378",
+  },
+];
+
+module.exports = nfts;
